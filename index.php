9be433e25ba9981ef5c52ea26c2797d262945ef9
marley ferreira <marleysheredder@gmail.com>
2013-07-13 02:49:09 -0300

    adicionando função de conexao postgres para o heroku

--- index.php
+++ index.php
@@ -26,6 +26,25 @@ $twig = new Twig_Environment($loader, array(
 
 $twig->addFilter('var_dump', new Twig_Filter_Function('var_dump'));
 */
+
+
+# This function reads your DATABASE_URL configuration automatically set by Heroku
+# the return value is a string that will work with pg_connect
+function pg_connection_string() {
+  // we will fill this out next
+}
+ 
+# Establish db connection
+$db = pg_connect(pg_connection_string());
+if (!$db) {
+   echo "Database connection error.";
+   exit;
+}
+ 
+$result = pg_query($db, "SELECT statement goes here");
+
+
+
 $authenticate = function ($app) {
     return function () use ($app) {
         if (!isset($_SESSION['user_id'])) {
