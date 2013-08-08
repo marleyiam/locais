89a6604ffcf34f1928e5807218c6136b1111220a
marley ferreira <marleysheredder@gmail.com>
2013-08-08 11:10:38 -0300

    avaliando DominsertedNode

--- js/main.js
+++ js/main.js
@@ -86,9 +86,9 @@
             try{
                 if(!$el.attr('href').toString().contains('google')){
                     str = $el.attr('href').toString();
-                    console.log(str)
+                    //console.log(str)
                     $el.attr('href',rootURL+str);  
-                    console.log($el.attr('href'))
+                    //console.log($el.attr('href'))
                 }
             }catch(e){
                
